8ef500ce5867027192a1eae6ea82fe462b4f5271
Paul Tyng <paul@paultyng.net>
2012-01-26 11:28:43 -0500

    Add extension method to module to not cache json responses in IE / Firefox

---
+++ src/Nancy/Json/ModuleJson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nancy.Json
+{
+	public static class ModuleJson
+	{
+		public static void DoNotCacheJsonResponse(this NancyModule module)
+		{
+			module.After.AddItemToEndOfPipeline(ctx =>
+			{
+				if (Json.IsJsonContentType(ctx.Response.ContentType))
+				{
+					//response is json, so set caching...
+					ctx.Response.Headers["Cache-Control"] = "no-cache, must-revalidate";
+					ctx.Response.Headers["Expires"] = "Mon, 26 Jul 1997 05:00:00 GMT";
+					ctx.Response.Headers["Pragma"] = "no-cache";
+				}
+			});
+		}
+	}
+}
